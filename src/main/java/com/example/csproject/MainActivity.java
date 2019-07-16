76417ae64b2af2c56c6a6e56532a2e429724dfbf
dancerwinter <jessica.manzano@obf.ateneo.edu>
2019-07-16 18:15:30 +0800

    added Add Class and class_row for the RecyclerView

--- src/main/java/com/example/csproject/MainActivity.java
+++ src/main/java/com/example/csproject/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-
+    @Click(R.id.button)
+    public void add() {
+        AddClass_.intent(this).start();
+    }
 
 }
